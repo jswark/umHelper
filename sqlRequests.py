5663e260f7ae6768068310f058067955f1bd385e
Kevin7985 <1298282@mail.ru>
2021-05-13 13:32:54 +0300

    sqlRequests.py debug: fixed auto update data from base

--- sqlRequests.py
+++ sqlRequests.py
@@ -1,11 +1,13 @@
 import pymysql
 
 con = pymysql.connect(host = "хост базы", user = "имя пользователя", password = "пароль пользователя", db = "название базы данных")
+con.autocommit(True)
 cur = con.cursor()
 
 def getUserGroup(userID):
 	cur.execute("SELECT `userGroup` FROM `users` WHERE `ID` = " + str(userID))
 	res = cur.fetchone()
+	print(res)
 	if (res == None):
 		return None
 	else:
