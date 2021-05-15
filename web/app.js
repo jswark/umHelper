d5e2b7e8ba8bcbf9d8ccb11e9f51b9ac4c5f9490
Kevin7985 <1298282@mail.ru>
2021-05-15 03:09:03 +0300

    web version: login page (server side) init

--- web/app.js
+++ web/app.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const mysql = require("mysql");
 
 const config = require("./config.js");
+const system = require("./system.js");
 
 const con = mysql.createConnection(config.mysql);
 const app = express();
@@ -12,6 +13,25 @@ app.use(express.static(__dirname + "/page"));
 
 con.connect((err) => {
 	if (err) throw err;
+
+	app.get("/", (req, res) => {
+		var cookies = system.getCookie(req.headers);
+		if (cookies.ui == null || cookies.us == null || cookies.uc == null) {
+			var options = { maxAge: -1 };
+			res.cookie("ui", "", options);
+			res.cookie("us", "", options);
+			res.cookie("uc", "", options);
+			fs.readFile("page/login.html", "utf-8", (err, data) => {
+				if (err) {
+					system.errorLog(err);
+					return res.end("404");
+				}
+				res.end(data);
+			});
+		} else {
+			// TO-DO проверка авторизации
+		}
+	});
 
 	app.listen(config.port, () => {
 		console.log("Server works at port " + config.port);
