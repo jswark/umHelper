78c748b5149d29b10c93af04dbbb05ca329a2b8d
Kevin7985 <1298282@mail.ru>
2021-05-15 03:07:45 +0300

    web version: system funcs file init

---
+++ web/system.js
@@ -0,0 +1,14 @@
+module.exports.getCookie = (headers) => {
+	var cookiesArr = headers.cookie.split("; ");
+	var cookies = {};
+	for (var i = 0; i < cookiesArr.length; i++) {
+		var temp = cookiesArr[i].split("=");
+		if (temp[1] == "") cookies[temp[0]] = null;
+		else cookies[temp[0]] = temp[1];
+	}
+	return cookies;
+}
+
+module.exports.errorLog = function(msg) => {
+	console.log(msg);
+}
